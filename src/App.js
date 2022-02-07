91b30d8c53c5e141083484de0bc9419a050aa722
Bhavya <bhavya0488.cse19@chitkara.edu.in>
2022-02-07 12:14:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Todolist from "./components/Todolist";
+import './App.css';
+
+const App =()=>{
+    return (
+        <>
+        <div className="todo-app">
+            <Todolist/>
+        </div>
+
+
+        </>
+    )
+}
+
+
+
+
+export default App;
